4632f1012fd681ed48eaabcae51c4a1299306597
HerKer02 <herker02@gmail.com>
2019-07-14 21:57:36 -0500

    working html

--- app/server.js
+++ app/server.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var path = require("path");
-
-var app = express();
+var bodyParser = require("body-parser")
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var app = express();
 
+var jsonParser = bodyParser.json();
+var urlencoderParser = bodyParser.urlencoded({extended: false});
+
+
+app.use(bodyParser.json({type: "application/*+json"}));
+app.use(bodyParser.raw({type: "application/vnd/custom-type"}));
+app.use(bodyParser.text({type: "text/html"}));
+
+require("./app/public/home.html")(app);
+
+// app.use(express.json());
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
